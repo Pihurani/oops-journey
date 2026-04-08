85d3b0414aabe116ec0ba8269c4955df21e261ff
Pihu Rani <pihurani16456@gmail.com>
2026-04-08 19:18:31 +0530

    Updated code: added constuctor

--- first.cpp
+++ first.cpp
@@ -5,16 +5,26 @@ class Student {
     string name;
     int age;
     int avg;
-
+    Student(){
+        name = "unknown";
+        age = 0;
+        avg = 0;
+    }
+    Student(string n, int a, int av){
+        name = n;
+        age = a;
+        avg = av;
+    }
+    void display(){
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Averge: " << avg << endl;
+    }
 };
 int main(){
-    Student s1;
-    s1.name = "pihu";
-    s1.age = 18;
-    s1.avg = 98;
-    cout << "Name: " << s1.name << endl;
-    cout << "Age: " << s1.age << endl;
-    cout << "Average: " << s1.avg << endl;
-
+    Student s1("pihu", 18, 98);
+    s1.display();
+    Student s2;
+    s2.display();
     return 0;
 }
